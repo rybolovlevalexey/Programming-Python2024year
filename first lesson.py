e92d2042b77dfca13e99caeb0f71d17a8f96f153
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2024-03-10 10:27:03 +0300

    Task C done

--- first lesson.py
+++ first lesson.py
@@ -1,23 +1,10 @@
-goal1, goal2 = map(int, input().split(":"))
-goal3, goal4 = map(int, input().split(":"))
-flag = int(input())
-first_guest = goal1 if flag == 2 else goal3
-second_guest = goal2 if flag == 1 else goal4
-
-first_goals = goal1 + goal3
-second_goals = goal2 + goal4
-if first_goals > second_goals or (first_goals == second_goals and first_guest > second_guest):
-    print(0)
-else:
-    dif = second_goals - first_goals
-    if flag == 1:
-        first_guest += dif
-        if first_guest > second_guest:
-            print(dif)
-        else:
-            print(dif + 1)
+ans = 0
+for i in range(int(input())):
+    count = int(input())
+    ans += count // 4
+    count = count % 4
+    if count in [0, 1, 2]:
+        ans += count
     else:
-        if first_guest > second_guest:
-            print(dif)
-        else:
-            print(dif + 1)
+        ans += 2
+print(ans)
